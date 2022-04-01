2e08185a5b97c6d99feeea224a516166a9b8e66e
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-04-01 12:29:42 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { MessageType } from "@adiwajshing/baileys";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
@@ -28,15 +28,16 @@ export default class Command extends BaseCommand {
 		if (!joined)
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
-		const images = [
-			"https://telegra.ph/file/a035a5447e04da6a771ed.jpg",
-			"https://telegra.ph/file/a4aac6c7a5df508fea9a4.jpg",
-			"https://telegra.ph/file/73c40cc251e52fbd2df0e.jpg",
-			"https://telegra.ph/file/42d4d2932912e24287dc2.jpg",
-			"https://data.whicdn.com/images/304776416/original.jpg",
-			"https://i.pinimg.com/564x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg",
+		const gifs = [
+			"https://c.tenor.com/NBFsEqKdSr8AAAPo/iron-man-mark50gif-beam.mp4",
+			"https://c.tenor.com/wkjOrQp6eJgAAAPo/i-am-iron-man-iron-man.mp4",
+			"https://c.tenor.com/gG3tX97uQaQAAAPo/spiderman-iron-man.mp4",
+			"https://c.tenor.com/bovTjaa8kM4AAAPo/iron-man-tony-stark.mp4",
+			"https://c.tenor.com/OUfRywXq_VoAAAPo/iron-man-door.mp4",
+			"https://c.tenor.com/qcJrw5CTlIcAAAPo/iron-man-awesome.mp4",
+			"https://c.tenor.com/Lamvbbk-XRUAAAPo/iron-man2-tony-stark.mp4",
 		];
-		const selected = images[Math.floor(Math.random() * images.length)];
+		const selected = gifs[Math.floor(Math.random() * gifs.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		const chats: any = this.client.chats
 			.all()
@@ -45,8 +46,9 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*💙「 ELAINA BROADCAST 」💙*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
-			this.client.sendMessage(chats[i], { url: selected }, MessageType.image, {
+			const text = `*⚡「ⓜⓞⓑⓐ ⓑⓒⓐⓢⓣ」⚡*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
+				mimetype: Mimetype.gif,
 				caption: `${text}`,
 				contextInfo: {
 					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
