aea7eb60c882b116ce4be17f7600757461e962ef
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-04-02 04:14:11 +0530

    Update Lyrics.ts

--- src/commands/music/Lyrics.ts
+++ src/commands/music/Lyrics.ts
@@ -3,41 +3,45 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-import Genius from 'genius-lyrics'
-import request from '../../lib/request'
+import yts from 'yt-search'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Lyrics from 'lyrics-monarch-api'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'lyrics',
-            description: 'Gives you the lyrics of the given song.',
+            description: 'Gives you lyrics with song playable on WhatsApp',
             category: 'media',
             aliases: ['ly'],
-            usage: `${client.config.prefix}lyrics [song_name]`,
-            baseXp: 40
+            usage: `${client.config.prefix}yts [term]`,
+            dm: false,
+            baseXp: 20
         })
     }
-    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!this.client.config.geniusKey)
-			return void M.reply("No Genius Access Token set.");
-        if (!joined) return void M.reply('Give me a song name to fetch the lyrics, Baka!')
-        const chitoge = joined.trim()
-        const Client = new Genius.Client(this.client.config.geniusKey)
-        const search = await Client.songs.search(chitoge)
-        if(search.error) return void M.reply(`Couldn't find any matching song results.`)
-        const lyrics = await search[0].lyrics()
-        let text = `🎀 *Title: ${search[0].title}*\n\n`
-            text += `🌐 *URL: ${search[0].url}*\n`
-            M.reply(
-							await request.buffer(search[0].image),
-							MessageType.image,
-							undefined,
-							undefined,
-							text,
-							undefined
-							// eslint-disable-next-line @typescript-eslint/no-explicit-any
-						).catch((reason: any) => M.reply(`${text}`));
-            await M.reply(lyrics)
-        };
-    } 
 
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void M.reply('🔎 Provide a search term')
+        const term = joined.trim()
+        const { videos } = await yts(term + ' lyrics song')
+        if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term *${term}*`)
+        const lyrics = new Lyrics()
+        const response = await lyrics.getLyrics(term)
+        if (!((response as any).status === 200)) return
+        this.client
+            .sendMessage(M.from, (response as any)?.data?.result?.lirik, MessageType.extendedText, {
+                quoted: M.WAMessage,
+                contextInfo: {
+                    externalAdReply: {
+                        title: `Search Term: ${term}`,
+                        body: `✨Made for you✨`,
+                        mediaType: 2,
+                        thumbnailUrl: videos[0].thumbnail,
+                        mediaUrl: videos[0].url
+                    }
+                }
+            })
+            .catch((reason: any) => M.reply(`❌ an error occupered, Reason: ${reason}`))
+    }
+}
