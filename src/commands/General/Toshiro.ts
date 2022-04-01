ffbb9a86a6e334cd4520ab4626977d1ba7599da6
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-04-01 11:38:38 +0530

    Update Toshiro.ts

--- src/commands/General/Toshiro.ts
+++ src/commands/General/Toshiro.ts
@@ -8,7 +8,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'savage',
+            command: 'moba',
             description: 'Displays info about savage.',
             category: 'general',
             usage: `${client.config.prefix}savage`
@@ -22,16 +22,19 @@ export default class Command extends BaseCommand {
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.image, {quoted:M.WAMessage,
             mimetype: Mimetype.jpeg,
-            caption: `🍭𝗵𝗲𝗹𝗹𝗼!🍃I'm Toshiro a learner/student & an upcoming developer in the future.
+            caption: `🍭𝗵𝗲𝗹𝗹𝗼!🍃🅘 🅐🅜 🅜🅞🅑🅐 🅘 🅛🅞🅥🅔 🅖🅘🅡🅛🅢 🅦🅘🅣🅗 🅟🅞🅔🅣🅡🅨.
             
 🍁𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
-Wa.me/+919609900020
-Wa.me/+918373017400
+Wa.me/+919711054140
+
+꧂𝐍𝐎𝐓𝐄;
+𝓑𝓮 𝓚𝓲𝓷𝓭 𝓑𝓮 𝓕𝓲𝓷𝓮
+𝓝𝓮𝓮𝓭 𝓐𝓷𝔂𝓽𝓱𝓲𝓷𝓰 𝓐𝓼𝓴
+𝓑𝓸𝓽 𝓸𝓻 𝓞𝓦𝓷𝓮𝓻 
 
 📮𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢;
-https://instagram.com/itz_toshiro12
-https://instagram.com/its_chey7
-
+https://instagram.com/_mobasshirachaudhary_
+https://instagram.com/falling_1392005
 🚀𝘿𝙞𝙨𝙘𝙤𝙧𝙙;
 ｟𝖢𝗈𝗆𝗂𝗇𝗀 𝖲𝗈𝗈𝗇｠
 
